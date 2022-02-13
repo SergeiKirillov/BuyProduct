c623c4f0fdf8a36647ff6b8d0ad6f7eff69fd8ad
user <user@LPC2AUTOITPROGM>
2022-02-13 09:52:36 +0600

    Add All ComboBox

--- BuyProduct/AddProductShopping.xaml.cs
+++ BuyProduct/AddProductShopping.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace BuyProduct
 {
@@ -22,11 +23,124 @@ namespace BuyProduct
         public AddProductShopping()
         {
             InitializeComponent();
+
+            dtShoping.Text = DateTime.Now.ToString();
+
+            LoadComboBox();
+
+        }
+
+        private void LoadComboBox()
+        {
+            using (var connection = new SQLiteConnection("Data Source = product.db"))
+            {
+                connection.Open();
+                string sqlExpression;
+                SQLiteCommand command;
+
+                #region Список категорий продуктов
+
+                sqlExpression = "select name from ProdCategoriaName order by name asc";
+                command = new SQLiteCommand(sqlExpression, connection);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string rt = reader.GetString(0);
+                            cmbCategoriaProduct.Items.Add(reader.GetString(0));
+                        }
+
+
+                    }
+
+                }
+
+                #endregion
+
+                #region Категория расходов
+                
+                sqlExpression = "Select CategoriaShopping FROM CatShop order by CategoriaShopping asc";
+                command = new SQLiteCommand(sqlExpression, connection);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            cmbCatShop.Items.Add(reader.GetString(0));
+                        }
+
+
+                    }
+
+                }
+                #endregion
+
+                #region Ед измерения массы
+                sqlExpression = "select Unit from ProdUnit order by Unit asc";
+                command = new SQLiteCommand(sqlExpression, connection);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            cmbProdUnit.Items.Add(reader.GetString(0));
+                        }
+
+
+                    }
+
+                }
+                #endregion
+
+
+                #region Магазин
+                sqlExpression = "select name,City from ShopName order by name asc";
+                command = new SQLiteCommand(sqlExpression, connection);
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            cmbShopName.Items.Add(reader.GetString(1)+","+ reader.GetString(0));
+                        }
+
+
+                    }
+
+                }
+                #endregion
+
+
+                connection.Close();
+                
+            }
+        }
+
+        private void LoadComboBox2()
+        {
+            using (var connection = new SQLiteConnection("Data Source = product.db"))
+            {
+                connection.Open();
+
+                
+
+            }
         }
 
         private void BtnSaveDB_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
+
+        
     }
 }
