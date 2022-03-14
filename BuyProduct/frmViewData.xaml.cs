64f3c1a161f2b04f4ef1c6c94e0a09c13c9371f0
user <user@LPC2AUTOITPROGM>
2022-03-14 11:07:19 +0600

    добавлена  форму просмотра покупок

--- BuyProduct/frmViewData.xaml.cs
+++ BuyProduct/frmViewData.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,74 @@ namespace BuyProduct
         {
             InitializeComponent();
             //начало просмотра данныъ
+            view1();
+            view2();
+        }
+
+        private void view1()
+        {
+            using (var connection = new SQLiteConnection("Data Source = product.db"))
+            {
+                connection.Open();
+                DataTable dTable = new DataTable();
+                string sqlQuery = "select productDateTime, ProductCategoriaName, productName, productRashod from PriceShops order by productDateTime ASC";
+
+                if (connection.State != ConnectionState.Open)
+                {
+                    System.Diagnostics.Debug.WriteLine("База не найдена!!");
+                    return;
+                }
+
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, connection);
+                adapter.Fill(dTable);
+
+                if (dTable.Rows.Count > 0)
+                {
+                    dgViewProduct.ItemsSource = dTable.DefaultView;
+                    //dgViewClass.ItemsSource = dTable.AsDataView();
+                    
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("В таблице нету данных для просмотра");
+                }
+
+
+            }
+        }
+        private void view2()
+        {
+            using (var connection = new SQLiteConnection("Data Source = product.db"))
+            {
+                connection.Open();
+                DataTable dTable = new DataTable();
+                string sqlQuery= "select DISTINCT ProductCategoriaName from ProductNames order by ProductCategoriaName";
+
+                if (connection.State!=ConnectionState.Open)
+                {
+                    System.Diagnostics.Debug.WriteLine("База не найдена!!");
+                    return;
+                }
+
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, connection);
+                adapter.Fill(dTable);
+
+                if (dTable.Rows.Count>0)
+                {
+                    dgViewClass.ItemsSource = dTable.DefaultView;
+                    //dgViewClass.ItemsSource = dTable.AsDataView();
+                    cmbViewCat.ItemsSource = dTable.DefaultView;
+                    cmbViewCat.DisplayMemberPath = "ProductCategoriaName";
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("В таблице нету данных для просмотра");
+                }
+
+
+            }
+   
+            
         }
     }
 }
